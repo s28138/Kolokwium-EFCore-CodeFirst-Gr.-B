c245f9d5a2e43aa451784fb01212020072e35be3
s28138 <s28138@pjwstk.edu.pl>
2026-06-07 11:41:10 +0200

    Stworzenie DBContext do aplikacji

---
+++ KolokwiumAPBDCodeFirst/Data/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using KolokwiumAPBDCodeFirst.Entities;
+using Microsoft.EntityFrameworkCore;
+namespace KolokwiumAPBDCodeFirst.Data;
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Product> Products => Set<Product>();
+    public DbSet<Order> Orders => Set<Order>();
+    public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+    public DbSet<Payment> Payments => Set<Payment>();
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<OrderItem>()
+            .HasKey(x => new { x.OrderId, x.ProductId });
+        modelBuilder.Entity<Order>()
+            .HasOne(x => x.User)
+            .WithMany(x => x.Orders)
+            .HasForeignKey(x => x.UserId);
+        modelBuilder.Entity<Payment>()
+            .HasOne(x => x.Order)
+            .WithMany(x => x.Payments)
+            .HasForeignKey(x => x.OrderId);
+        modelBuilder.Entity<OrderItem>()
+            .HasOne(x => x.Order)
+            .WithMany(x => x.OrderItems)
+            .HasForeignKey(x => x.OrderId);
+        modelBuilder.Entity<OrderItem>()
+            .HasOne(x => x.Product)
+            .WithMany(x => x.OrderItems)
+            .HasForeignKey(x => x.ProductId);
+    }
+}
