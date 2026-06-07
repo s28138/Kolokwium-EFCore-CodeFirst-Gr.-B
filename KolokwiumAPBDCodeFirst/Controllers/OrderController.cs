ddbffdfbb766bd283775a68e5d19f3e8eb4ba0ff
s28138 <s28138@pjwstk.edu.pl>
2026-06-07 11:42:13 +0200

    Controller - utworzenie

---
+++ KolokwiumAPBDCodeFirst/Controllers/OrderController.cs
@@ -0,0 +1,34 @@
+﻿using KolokwiumAPBDCodeFirst.DTOs;
+
+using KolokwiumAPBDCodeFirst.Services;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace KolokwiumAPBDCodeFirst.Controllers;
+[ApiController]
+[Route("api/orders")]
+public class OrdersController : ControllerBase
+{
+    private readonly IOrderService _service;
+    public OrdersController(IOrderService service)
+    {
+        _service = service;
+    }
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetOrder(int id)
+    {
+        var result = await _service.GetOrder(id);
+        if (result == null)
+            return NotFound();
+        return Ok(result);
+    }
+    [HttpPut]
+    public async Task<IActionResult> ProcessOrder(
+        UpdateOrderDto dto)
+    {
+        var result = await _service.ProcessOrder(dto);
+        if (!result)
+            return NotFound();
+        return Ok("Order processed");
+    }
+}
