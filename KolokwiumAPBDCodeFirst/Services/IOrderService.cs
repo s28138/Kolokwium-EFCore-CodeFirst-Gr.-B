9fb95dc1a43b78f4f5e3b7cf9c53d16ebeee0058
s28138 <s28138@pjwstk.edu.pl>
2026-06-07 11:42:26 +0200

    Service - utworzenie

---
+++ KolokwiumAPBDCodeFirst/Services/IOrderService.cs
@@ -0,0 +1,9 @@
+﻿using KolokwiumAPBDCodeFirst.DTOs;
+
+namespace KolokwiumAPBDCodeFirst.Services;
+
+public interface IOrderService
+{
+    Task<object?> GetOrder(int orderId);
+    Task<bool> ProcessOrder(UpdateOrderDto dto);
+}
